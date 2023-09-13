1cf0b3697a109a8b1876613c007d70ba3b15e326
SonicYeager <kimstone.yeager@gmail.com>
2023-09-13 21:26:43 +0200

    slight adjustements for learning

--- MyMauiApp/App.xaml.cs
+++ MyMauiApp/App.xaml.cs
@@ -1,12 +1,37 @@
-﻿namespace MyMauiApp
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿using Microsoft.Extensions.Logging;
 
-            MainPage = new AppShell();
-        }
+namespace MyMauiApp;
+
+public partial class App : Application
+{
+    private readonly ILogger<App> _logger;
+
+    public App(ILogger<App> logger)
+    {
+        _logger = logger;
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+
+    protected override void OnStart()
+    {
+        //TODO provide startup code
+        _logger.LogInformation("App Startup");
+        base.OnStart();
+    }
+
+    protected override void OnResume()
+    {
+        //TODO provide resume code
+        _logger.LogInformation("App Resume");
+        base.OnResume();
+    }
+
+    protected override void OnSleep()
+    {
+        //TODO provide sleep code
+        _logger.LogInformation("App Sleep");
+        base.OnSleep();
     }
 }
