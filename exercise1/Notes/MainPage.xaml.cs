729dc2c56035e754acc53ecdc313cf546b8531e8
SonicYeager <kimstone.yeager@gmail.com>
2023-09-14 17:34:41 +0200

    xaml basics

---
+++ exercise1/Notes/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Notes;
+
+public partial class MainPage : ContentPage
+{
+    private readonly string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        if (File.Exists(_fileName)) Editor.Text = File.ReadAllText(_fileName);
+    }
+
+    private void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, Editor.Text);
+    }
+
+    private void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName)) File.Delete(_fileName);
+        Editor.Text = string.Empty;
+    }
+}
